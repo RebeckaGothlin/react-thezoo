ced4e25bf6566d154d6cc5b3e7f2ae1cfce2e7fe
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-08-28 14:51:23 +0200

    :sparkles: add content to files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,20 @@
+import "../style/home.scss";
+import { Link } from "react-router-dom";
+import logo from "/home-logo.png";
+
 export const Home = () => {
-    return <>Home</>
-}
+  return (
+    <>
+      <div className="hero-img">
+        <div className="hero-info">
+          <div className="logo-container">
+            <img src={logo} alt="the zoo logo" width={375} />
+          </div>
+          <Link to="/animals">
+            <button>Våra djur</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
