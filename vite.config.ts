ff61100332f76288797782c103d02c43527f5242
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-08-29 10:24:53 +0200

    🚀 add deploy base again

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/the-zoo-RebeckaGothlin/",
-  plugins: [react()],
+   plugins: [react()],
+   base: '/the-zoo-RebeckaGothlin/',
 })
